edd5cbcea944fed03473c6a4d49069bd126ce176
Mateus Medeiros Garcia <mateusm.garcia0@gmail.com>
2023-01-29 19:36:01 -0300

    add - logic

--- src/main/java/com/bantads/accountOrchestrator/config/MessagingConfig.java
+++ src/main/java/com/bantads/accountOrchestrator/config/MessagingConfig.java
@@ -7,7 +7,9 @@ import org.springframework.amqp.rabbit.core.RabbitTemplate;
 import org.springframework.amqp.support.converter.Jackson2JsonMessageConverter;
 import org.springframework.amqp.support.converter.MessageConverter;
 import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
 
+@Configuration
 public class MessagingConfig {
 
     public static final String QUEUE = "account-orchestrator-queue";
@@ -29,6 +31,7 @@ public class MessagingConfig {
         return BindingBuilder.bind(queue).to(exchange).with(ROUTING_KEY);
     }
 
+    @Bean
     public MessageConverter converter(){
         return new Jackson2JsonMessageConverter();
     }
--- src/main/java/com/bantads/accountOrchestrator/dto/AccountStatus.java
+++ src/main/java/com/bantads/accountOrchestrator/dto/AccountStatus.java
@@ -2,10 +2,12 @@ package com.bantads.accountOrchestrator.dto;
 
 import lombok.*;
 
+import java.io.Serializable;
+
 @Data
 @AllArgsConstructor
 @NoArgsConstructor
-public class AccountStatus {
+public class AccountStatus implements Serializable {
     private Account account;
     private String status;
     private String message;
