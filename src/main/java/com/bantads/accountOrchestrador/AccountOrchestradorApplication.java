380c603101383a41b6db355497d69fd35de27c52
Mateus Medeiros Garcia <mateusm.garcia0@gmail.com>
2023-01-29 15:33:39 -0300

    initial commit

---
+++ src/main/java/com/bantads/accountOrchestrador/AccountOrchestradorApplication.java
@@ -0,0 +1,13 @@
+package com.bantads.accountOrchestrador;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AccountOrchestradorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AccountOrchestradorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/bantads/accountOrchestrador/AccountOrchestradorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bantads.accountOrchestrador;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountOrchestradorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
